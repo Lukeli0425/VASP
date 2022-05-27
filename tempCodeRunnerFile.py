a40e2295e417ef702aa4aa4cb0f9dc0893583a55
TIANAO LI <lta19@mails.tsinghua.edu.cn>
2022-05-27 15:01:25 +0800

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-r') as f:
-        task2_gt = json.load(f)
-    task2_pred = test_task2('./test_offline/task2')
-    task2_acc = utils.calc_accuracy(task2_gt,task2_pred)
-    print('accuracy for task2 is:',task2_acc)  
